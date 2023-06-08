c7105cbf26341dc0a1bcc9b79fd6e4f340572ae7
h-osawa <h-osawa@ruby-dev.jp>
2023-06-08 14:52:14 +0900

    gem:letter_opener_webのインストール

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
     passwords: 'users/passwords'
   }
+
   resources :books
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
