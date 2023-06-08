dc624852f7102954df95b5e6b5081f6e3dde989e
h-osawa <h-osawa@ruby-dev.jp>
2023-06-08 14:16:18 +0900

    controllersとviewsを作成
    不要なのは後から削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
+  }
   resources :books
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
   # Defines the root path route ("/")
