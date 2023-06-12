57e4edc0e383d93d919a0ebab3907deeed14cbb9
h-osawa <h-osawa@ruby-dev.jp>
2023-06-12 17:23:44 +0900

    rubocopとerblintの実行・修正

--- Gemfile
+++ Gemfile
@@ -57,8 +57,8 @@ end
 
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
-  gem 'web-console'
   gem 'letter_opener_web', '~> 2.0'
+  gem 'web-console'
   # Add speed badges [https://github.com/MiniProfiler/rack-mini-profiler]
   # gem "rack-mini-profiler"
 
@@ -80,6 +80,6 @@ group :test do
 end
 
 gem 'carrierwave'
-gem 'kaminari'
 gem 'devise'
 gem 'devise-i18n'
+gem 'kaminari'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,11 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_in_path_for(resource)
+  def after_sign_in_path_for
     books_path
   end
 
-  def after_sign_out_path_for(resource)
+  def after_sign_out_path_for
     new_user_session_path
   end
 
@@ -16,6 +16,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :zipcode, :address, :introduction_text])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name zipcode address introduction_text])
   end
 end
