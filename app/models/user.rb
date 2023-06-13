2023b6be56b524f60fabda7a2b0b7bafeda5227f
h-osawa <h-osawa@ruby-dev.jp>
2023-06-13 15:04:14 +0900

    アップロード可能なファイルタイプを制限

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :icon
+  validates :icon, content_type: ['image/png', 'image/jpeg', 'image/gif']
 end
