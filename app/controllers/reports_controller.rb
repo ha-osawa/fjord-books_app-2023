9e8b9621a51ac61c74c23523babb3058d1d8e9ea
h-osawa <h-osawa@ruby-dev.jp>
2023-06-14 15:34:07 +0900

    scaffoldで作成された不要なファイルやjsonの記述を削除

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -24,38 +24,28 @@ class ReportsController < ApplicationController
   def create
     @report = current_user.reports.build(report_params)
 
-    respond_to do |format|
       if @report.save
-        format.html { redirect_to report_url(@report), notice: "Report was successfully created." }
-        format.json { render :show, status: :created, location: @report }
+        redirect_to report_url(@report), notice: "Report was successfully created."
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @report.errors, status: :unprocessable_entity }
+        render :new, status: :unprocessable_entity
       end
-    end
   end
 
   # PATCH/PUT /reports/1 or /reports/1.json
   def update
-    respond_to do |format|
+    
       if @report.update(report_params)
-        format.html { redirect_to report_url(@report), notice: "Report was successfully updated." }
-        format.json { render :show, status: :ok, location: @report }
+        redirect_to report_url(@report), notice: "Report was successfully updated." 
       else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @report.errors, status: :unprocessable_entity }
+        render :edit, status: :unprocessable_entity 
       end
-    end
   end
 
   # DELETE /reports/1 or /reports/1.json
   def destroy
     @report.destroy
 
-    respond_to do |format|
-      format.html { redirect_to reports_url, notice: "Report was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to reports_url, notice: "Report was successfully destroyed." 
   end
 
   private
