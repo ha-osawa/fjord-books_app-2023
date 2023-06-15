a1c93413253cb5331c465a422fc8b2cf2c584ac1
h-osawa <h-osawa@ruby-dev.jp>
2023-06-15 10:23:37 +0900

    rubocopとerblintの実行・修正

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class ReportsController < ApplicationController
-  before_action :set_report, only: %i[ show edit update destroy ]
-  before_action :ensure_user, only: %i[ edit update destroy ]
+  before_action :set_report, only: %i[show edit update destroy]
+  before_action :ensure_user, only: %i[edit update destroy]
 
   # GET /reports or /reports.json
   def index
@@ -8,8 +10,7 @@ class ReportsController < ApplicationController
   end
 
   # GET /reports/1 or /reports/1.json
-  def show
-  end
+  def show; end
 
   # GET /reports/new
   def new
@@ -17,51 +18,50 @@ class ReportsController < ApplicationController
   end
 
   # GET /reports/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /reports or /reports.json
   def create
     @report = current_user.reports.build(report_params)
 
-      if @report.save
-        redirect_to report_url(@report), notice: t('controllers.common.notice_create', name: Report.model_name.human)
-      else
-        render :new, status: :unprocessable_entity
-      end
+    if @report.save
+      redirect_to report_url(@report), notice: t('controllers.common.notice_create', name: Report.model_name.human)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   # PATCH/PUT /reports/1 or /reports/1.json
   def update
-    
-      if @report.update(report_params)
-        redirect_to report_url(@report), notice: t('controllers.common.notice_update', name: Report.model_name.human) 
-      else
-        render :edit, status: :unprocessable_entity 
-      end
+    if @report.update(report_params)
+      redirect_to report_url(@report), notice: t('controllers.common.notice_update', name: Report.model_name.human)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   # DELETE /reports/1 or /reports/1.json
   def destroy
     @report.destroy
 
-    redirect_to reports_url, notice: t('controllers.common.notice_destroy', name: Report.model_name.human) 
+    redirect_to reports_url, notice: t('controllers.common.notice_destroy', name: Report.model_name.human)
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_report
-      @report = Report.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def report_params
-      params.require(:report).permit(:title, :content)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_report
+    @report = Report.find(params[:id])
+  end
 
-    def ensure_user
-      @current_user_reports = current_user.reports
-      @target_report = @current_user_reports.find_by(id: params[:id])
-      redirect_to report_path(params[:id]), alert: t('controllers.common.edit_destroy_restriction', name: Report.model_name.human) unless @target_report
-    end
+  # Only allow a list of trusted parameters through.
+  def report_params
+    params.require(:report).permit(:title, :content)
+  end
+
+  def ensure_user
+    @current_user_reports = current_user.reports
+    @target_report = @current_user_reports.find_by(id: params[:id])
+    redirect_to report_path(params[:id]), alert: t('controllers.common.edit_destroy_restriction', name: Report.model_name.human) unless @target_report
+  end
 end
