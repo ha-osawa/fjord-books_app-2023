61cc31982742e30f6459d061c4a35ef705d394e7
h-osawa <h-osawa@ruby-dev.jp>
2023-06-22 10:51:50 +0900

    after_sing_*_path_forをprivate扱いにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  private
+
   def after_sign_in_path_for(_resources)
     books_path
   end
