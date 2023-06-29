202583c0d57457aea883dfc7586d20eff369270e
h-osawa <h-osawa@ruby-dev.jp>
2023-06-29 16:19:37 +0900

    rubocopの実行・修正

--- app/models/mention.rb
+++ app/models/mention.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class Mention < ApplicationRecord
-  belongs_to :mentioning_report, class_name: "Report"
-  belongs_to :mentioned_report, class_name: "Report"
+  belongs_to :mentioning_report, class_name: 'Report'
+  belongs_to :mentioned_report, class_name: 'Report'
 
   validates :mentioning_report_id, presence: true, uniqueness: { scope: :mentioned_report }
   validates :mentioned_report_id, presence: true, uniqueness: { scope: :mentioning_report }
--- app/models/report.rb
+++ app/models/report.rb
@@ -1,15 +1,16 @@
 # frozen_string_literal: true
+
 require 'uri'
 
 class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
 
-  has_many :mentionings, class_name: "Mention", foreign_key: :mentioned_report_id, dependent: :destroy
-  has_many :mentioneds, class_name: "Mention", foreign_key: :mentioning_report_id, dependent: :destroy
+  has_many :mentionings, class_name: 'Mention', foreign_key: :mentioned_report_id, inverse_of: :mentioned_report, dependent: :destroy
+  has_many :mentioneds, class_name: 'Mention', foreign_key: :mentioning_report_id, inverse_of: :mentioning_report, dependent: :destroy
 
   has_many :mentioning_reports, through: :mentionings, source: :mentioning_report
-  has_many :mentioned_reports, through: :mentioneds,  source: :mentioned_report
+  has_many :mentioned_reports, through: :mentioneds, source: :mentioned_report
 
   validates :title, presence: true
   validates :content, presence: true
@@ -23,46 +24,46 @@ class Report < ApplicationRecord
   end
 
   def mentioning?
-    content.match?(/http:\/\/localhost:3000\/reports\/[0-9]+/) || self.mentionings
+    content.match?(%r{http://localhost:3000/reports/[0-9]+}) || mentionings
   end
 
   def extract_mentioning_report_id
-    content.scan(/http:\/\/localhost:3000\/reports\/[0-9]+/).map do |uri|
-      URI.parse(uri).path.match(/[^\/reports\/][0-9]*/).to_s.to_i
+    content.scan(%r{http://localhost:3000/reports/[0-9]+}).map do |uri|
+      URI.parse(uri).path.match(%r{[^/reports/][0-9]*}).to_s.to_i
     end
   end
 
   def create_mentioning_reports
-    self.extract_mentioning_report_id.each do |mentioning_id|
-      self.mentionings.create!(mentioning_report_id: mentioning_id)
+    extract_mentioning_report_id.each do |mentioning_id|
+      mentionings.create!(mentioning_report_id: mentioning_id)
     end
   end
 
   def update_mantioning_reports
-    update_mentioning_report_ids = self.extract_mentioning_report_id - self.mentionings.pluck(:mentioning_report_id)
-    self.mentionings.where.not(mentioning_report_id: self.extract_mentioning_report_id ).destroy_all
+    update_mentioning_report_ids = extract_mentioning_report_id - mentionings.pluck(:mentioning_report_id)
+    mentionings.where.not(mentioning_report_id: extract_mentioning_report_id).destroy_all
     update_mentioning_report_ids.each do |mentioning_id|
-      self.mentionings.create_or_find_by!(mentioning_report_id: mentioning_id)
+      mentionings.create_or_find_by!(mentioning_report_id: mentioning_id)
     end
   end
 
   def execute_create_transaction
     ActiveRecord::Base.transaction do
-      self.save!
-      self.create_mentioning_reports if self.mentioning?
+      save!
+      create_mentioning_reports if mentioning?
     end
     true
-    rescue ActiveRecord::RecordInvalid
-      false
+  rescue ActiveRecord::RecordInvalid
+    false
   end
 
   def execute_update_transaction(report_params)
     ActiveRecord::Base.transaction do
-      self.update!(report_params)
-      self.update_mantioning_reports if self.mentioning?
+      update!(report_params)
+      update_mantioning_reports if mentioning?
     end
     true
-    rescue ActiveRecord::RecordInvalid
-      false
+  rescue ActiveRecord::RecordInvalid
+    false
   end
 end
